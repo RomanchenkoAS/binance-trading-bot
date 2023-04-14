b842a932faaf617e675f99184ca57897b7b92ca4
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-14 16:55:04 +0600

    recieved and framed data

--- api.py
+++ api.py
@@ -5,4 +5,21 @@ import datetime
 
 currency_pair = 'btcusd'
 
-URL = f'https://www.bitstamp.net/api/v2/ohlc/${currency_pair}/'
+URL = f'https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/'
+
+params = {
+    'step' : 60,
+    'limit' : 10 # 1 .. 1000
+}
+
+data = requests.get(URL, params=params).json()
+
+# for line in data['data']['ohlc']:
+#     print(line)
+    
+data = data['data']['ohlc']
+
+df = pd.DataFrame(data)
+
+print(df)
+
