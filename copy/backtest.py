3c6a0e81ad245c7e8fb19a9366b94e0beccaeb2b
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-18 19:07:51 +0600

    rearrange

--- copy/backtest.py
+++ copy/backtest.py
@@ -1,31 +1,31 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime
 import vectorbt as vbt
 
-btc_price = pd.read_csv("tutorial.csv")[["timestamp","close"]]
-btc_price["date"] = pd.to_datetime( btc_price["timestamp"], unit = "s")
+num = 10
+metric = "total_return"
 
+btc_price = pd.read_csv("tutorial.csv")[["timestamp", "close"]]
+btc_price["date"] = pd.to_datetime(btc_price["timestamp"], unit="s")
 btc_price = btc_price.set_index("date")["close"]
 
-num = 10
-rsi = vbt.RSI.run(btc_price, window = 14, short_name="rsi")
-entry_points = np.linspace(1,45, num=num)
-exit_points = np.linspace(55,99, num=num)
-grid = np.array(np.meshgrid(entry_points, exit_points)).T.reshape(-1,2)
+
+rsi = vbt.RSI.run(btc_price, window=14, short_name="rsi")
+entry_points = np.linspace(1, 45, num=num)
+exit_points = np.linspace(55, 99, num=num)
+grid = np.array(np.meshgrid(entry_points, exit_points)).T.reshape(-1, 2)
 entries = rsi.rsi_crossed_below(list(grid[:, [0]]))
 exits = rsi.rsi_crossed_above(list(grid[:, [1]]))
 pf = vbt.Portfolio.from_signals(btc_price, entries, exits)
 
-metric = "total_return"
 pf_perf = pf.deep_getattr(metric)
 
 pf_perf_matrix = pf_perf.vbt.unstack_to_df(
-        index_levels = "rsi_crossed_above",
-        column_levels = "rsi_crossed_below")
+    index_levels="rsi_crossed_above",
+    column_levels="rsi_crossed_below")
 
 print(pf_perf_matrix)
 
 pf_perf_matrix.vbt.heatmap(
-        xaxis_title = "entry",
-        yaxis_title="exit").show()
+    xaxis_title="entry",
+    yaxis_title="exit").show()
--- copy/scraping.py
+++ copy/scraping.py
@@ -1,8 +1,5 @@
-import json
 import requests
 import pandas as pd
-import datetime
-
 
 currency_pair = "btcusd"
 url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
@@ -10,40 +7,41 @@ url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
 start = "2021-01-01"
 end = "2021-01-02"
 
-dates = pd.date_range(start, end, freq = "6H")
-dates  = [ int(x.value/10**9) for x in list(dates)]
+dates = pd.date_range(start, end, freq="6H")
+dates = [int(x.value/10**9) for x in list(dates)]
 
 print(dates)
 
 master_data = []
 
 for first, last in zip(dates, dates[1:]):
-    print(first,last)
+    print(first, last)
 
     params = {
-            "step":60,
-            "limit":1000,
-            "start":first,
-            "end":last,
-            }
+        "step": 60,  # seconds
+        "limit": 1000,  # 1..1000
+        "start": first,
+        "end": last,
+    }
 
-    data = requests.get(url, params = params)
-
+    data = requests.get(url, params=params)
 
     data = data.json()["data"]["ohlc"]
 
     master_data += data
-    
-    
+
+# Clear up the dataframe
 df = pd.DataFrame(master_data)
 df = df.drop_duplicates()
 
 df["timestamp"] = df["timestamp"].astype(int)
 df = df.sort_values(by="timestamp")
 
-df = df[ df["timestamp"] >= dates[0] ]
-df = df[ df["timestamp"] < dates[-1] ]
+# Filter resulting dates to cut off extra ones
+df = df[df["timestamp"] >= dates[0]]
+df = df[df["timestamp"] < dates[-1]]
 
 print(df)
 
+# Write results to csv
 df.to_csv("tutorial.csv", index=False)
