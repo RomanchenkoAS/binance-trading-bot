444e08691bfea28c7ff1cb1b7eb1aa7e1a314d8f
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-21 16:58:34 +0600

    upd

--- bot.py
+++ bot.py
@@ -1,16 +1,57 @@
-# Requirements: python-decouple python-binance pandas pandas-ta 
-# API : testnet.binance.vision 
+# Requirements: python-decouple python-binance pandas pandas-ta
+# API : testnet.binance.vision
 
 from decouple import config
 from binance.client import Client
+import pandas as pd
+import pandas_ta as ta
+import time
+import json
 
-print(config("API_KEY"))
-print(config("SECRET_KEY"))
+# testnet = True means all the trading is virtual
+client = Client(config("API_KEY"), config("SECRET_KEY"), testnet=True)
 
-# testnet = True means all the trading is virtual 
-client = Client(config("API_KEY"), config("SECRET_KEY"), testnet = True)
-
-
-
+# Balance check
+# balance = client.get_asset_balance(asset="BTC")
+# print(balance)
 
 
+def fetch_klines(asset):
+
+    klines = client.get_historical_klines(
+        asset, Client.KLINE_INTERVAL_1MINUTE, "1 hour ago UTC")
+
+    klines = [[x[0], float(x[4])] for x in klines]
+
+    klines = pd.DataFrame(klines, columns=["time", "price"])
+    klines["time"] = pd.to_datetime(klines["time"], unit="ms")
+
+    return klines
+
+
+def get_rsi(asset):
+
+    klines = fetch_klines(asset)
+    klines["rsi"] = ta.rsi(close=klines["price"], length=14)
+
+    return klines["rsi"].iloc[-1]
+
+
+# for i in range(0, 10):
+#     rsi = get_rsi("BTCUSDT")
+#     print(rsi)
+#     time.sleep(5)
+
+def create_account():
+    
+    pass
+
+    account = {
+        "is_buying":True,
+        "assets":{},
+    }
+    
+    with open("bot_account.json", "w") as f:
+        f.write( json.dumps(account))
+        
+create_account()
