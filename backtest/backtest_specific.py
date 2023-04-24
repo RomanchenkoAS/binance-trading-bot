40732e1fd366da8ddbca5b77c4d6ac0316177f04
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-24 13:29:52 +0600

    upd

--- backtest/backtest_specific.py
+++ backtest/backtest_specific.py
@@ -7,7 +7,7 @@ btc_price = pd.read_csv("data_1m.csv")[["timestamp", "close"]]
 btc_price["date"] = pd.to_datetime(btc_price["timestamp"], unit="s")
 btc_price = btc_price.set_index("date")["close"]
 
-for window in range(1, 300): #optimize range
+for window in range(100, 101): #optimize range (window = 4 gives 16.64%)
     # VectorBT part
     rsi = vbt.RSI.run(btc_price, window=window, short_name="rsi")
 
@@ -22,9 +22,9 @@ for window in range(1, 300): #optimize range
 
     # Print
     stats = pf.stats()
-    # print(stats)
+    print(stats)
 
     total_return = stats[5] 
     total_trades = stats[11]
 
-    print(f"window = {window}: return = {total_return:.3f} | trades = {total_trades}")
+    print(f"window = {window}: return = {total_return:.2f} \t trades = {total_trades} \t")
